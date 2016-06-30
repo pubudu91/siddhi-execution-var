e7649d418f49113b4db9da8c6a29ac3c22f59939
Nipuna-Sankalpa <nipuna92@yahoo.com>
2016-06-30 10:08:32 +0530

    monte carlo simulation part implemented

---
+++ src/main/java/org/wso2/siddhi/extension/var/batchmode/MonteCarloSimulation.java
@@ -0,0 +1,7 @@
+package org.wso2.siddhi.extension.var.batchmode;
+
+/**
+ * Created by flash on 6/27/16.
+ */
+public class MonteCarloSimulation {
+}
---
+++ src/main/java/org/wso2/siddhi/extension/var/batchmode/MonteCarloVarCalculator.java
@@ -0,0 +1,7 @@
+package org.wso2.siddhi.extension.var.batchmode;
+
+/**
+ * Created by flash on 6/29/16.
+ */
+public class MonteCarloVarCalculator {
+}
