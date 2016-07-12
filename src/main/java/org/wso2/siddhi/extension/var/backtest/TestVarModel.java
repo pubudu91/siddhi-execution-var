2f527b4314352f2e21e69a1cfaeb1e11e30089ad
Nipuna-Sankalpa <nipuna92@yahoo.com>
2016-07-12 22:47:50 +0530

    back test implement pull data fetching method

--- src/main/java/org/wso2/siddhi/extension/var/backtest/TestVarModel.java
+++ src/main/java/org/wso2/siddhi/extension/var/backtest/TestVarModel.java
@@ -1,36 +1,60 @@
 package org.wso2.siddhi.extension.var.backtest;
 
 import org.wso2.siddhi.extension.var.models.Asset;
-import org.wso2.siddhi.extension.var.realtime.VaRPortfolioCalc;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Created by flash on 7/12/16.
  */
-public class TestVarModel {
+public abstract class TestVarModel {
     private double var[];
     private double actualValue[];
     private int sampleSize = 250;
-    private VaRPortfolioCalc calcInstance;
+    private Map<String, Asset> portfolio = null;
 
-    public TestVarModel(VaRPortfolioCalc calcInstance) {
-        this.calcInstance = calcInstance;
+    public TestVarModel(int sampleSize, Map<String, Asset> portfolio) {
+        this.sampleSize = sampleSize;
+        this.portfolio = portfolio;
     }
 
-    public void generateVar() {
-        Map<String, Asset> assets = new HashMap<>();
-        int limit = 250;
-        double confidenceInterval = 0.95;
-        Object[] input = null;
-
-        calcInstance.calculateValueAtRisk(input);
+    public int getSampleSize() {
+        return sampleSize;
     }
 
-    public void generateOriginalLoss() {
-
+    public void setSampleSize(int sampleSize) {
+        this.sampleSize = sampleSize;
     }
 
+    protected abstract double[] calculateVar();
+
+    /**
+     * send sample set e.g:- by sending 1, calculation will be done from first
+     * set of sample size of the price list
+     *
+     * @param priceList
+     * @param sampleSet
+     * @return
+     */
+    protected double[] calculateOriginal(Map<String, ArrayList<Double>> priceList, int sampleSet) {
+        actualValue = new double[sampleSize];
+        ArrayList<Double> priceListTemp = null;
+        int sharesAmount = 0, endOfList = 0;
+
+        String[] key = this.portfolio.keySet().toArray(new String[this.portfolio.size()]);
+        for (int i = 0; i < sampleSize; i++) {
+            actualValue[i] = 0;
+        }
+        for (int i = 0; i < key.length; i++) {
+            priceListTemp = priceList.get(key[i]);
+            endOfList = (sampleSet * sampleSize) > priceListTemp.size() ? priceListTemp.size() : sampleSet * sampleSize;
+            sharesAmount = portfolio.get(key[i]).getNumberOfShares();
+            for (int j = sampleSize * (sampleSet - 1); j < endOfList; j++) {
+                actualValue[i] += sharesAmount * priceListTemp.get(i);
+            }
+        }
+        return null;
+    }
 
 }
