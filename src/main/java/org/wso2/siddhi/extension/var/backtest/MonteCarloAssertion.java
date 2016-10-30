42f00742879aa979fdaa4f36a386f9e54a0dc55e
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2016-10-30 19:22:34 +0530

    backtest framework changed - now inputs are randomly fed

--- src/main/java/org/wso2/siddhi/extension/var/backtest/MonteCarloAssertion.java
+++ src/main/java/org/wso2/siddhi/extension/var/backtest/MonteCarloAssertion.java
@@ -6,14 +6,16 @@ import org.wso2.siddhi.extension.var.models.Portfolio;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by flash on 7/14/16.
  */
 public class MonteCarloAssertion extends VarModelAssertion {
+    Random rand = new Random();
 
-    public MonteCarloAssertion(int sampleSize, Map<String, Integer> portfolio, double confidenceInterval, int batchSize,int sampleSetNumber) {
-        super(sampleSize, portfolio, confidenceInterval, batchSize,sampleSetNumber);
+    public MonteCarloAssertion(int sampleSize, Map<String, Integer> portfolio, double confidenceInterval, int batchSize, int sampleSetNumber) {
+        super(sampleSize, portfolio, confidenceInterval, batchSize, sampleSetNumber);
     }
 
     @Override
@@ -26,12 +28,13 @@ public class MonteCarloAssertion extends VarModelAssertion {
         this.setHistoricalValues(calculator);
 
         for (int i = 0; i < tempVar.length; i++) {
-            for (int j = 0; j < key.length; j++) {
-                Object input[] = {key[j], priceLists.get(key[j]).get(i + this.getBatchSize() + (this.getSampleSize() *
-                        this.getSampleSetNumber()))};
-                calculator.addEvent(input);
-                calculator.removeEvent(key[j]);
-            }
+            int j = rand.nextInt(key.length - 1);
+            Object input[] = {key[j], priceLists.get(key[j]).get(i + this.getBatchSize() + (this.getSampleSize() *
+                    this.getSampleSetNumber()))};
+            calculator.addEvent(input);
+            calculator.removeEvent(key[j]);
+
+            _portfolio.setIncomingEventLabel((String) input[0]);
             tempVar[i] = (Double) calculator.processData(_portfolio);
         }
         return tempVar;
--- src/test/java/org/wso2/siddhi/extension/var/backtest/MonteCarloAssertionTest.java
+++ src/test/java/org/wso2/siddhi/extension/var/backtest/MonteCarloAssertionTest.java
@@ -18,11 +18,10 @@ public class MonteCarloAssertionTest {
         int i = 0, passedCount = 0;
         Random rnd = new Random();
         Map<String, Integer> portfolio = new HashMap<>();
-//        for (int j = 0; j < 10; j++) {
             portfolio.put("IBM", rnd.nextInt(100));
             portfolio.put("GE", rnd.nextInt(300));
             portfolio.put("XOM", rnd.nextInt(500));
-        System.out.println("Nipuns Sanklapa");
+
             while (true) {
                 calc = new MonteCarloAssertion(500, portfolio, 0.95, 251, i);
                 try {
@@ -38,6 +37,6 @@ public class MonteCarloAssertionTest {
 
             System.out.println(passedCount);
             System.out.println("Passed Probability:" + passedCount / i);
-//        }
+
     }
 }
