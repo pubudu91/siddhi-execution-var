ef5824165dcaf7f04db4acc8d1c59781d4ddb443
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2016-11-07 11:36:41 +0530

    project step_1 ultimate update

--- src/main/java/org/wso2/siddhi/extension/var/backtest/VarModelAssertion.java
+++ src/main/java/org/wso2/siddhi/extension/var/backtest/VarModelAssertion.java
@@ -122,8 +122,6 @@ public abstract class VarModelAssertion {
 
         for (int i = 0; i < sampleSize - 1; i++) {
             actualLoss = this.actualValue[i + 1] - this.actualValue[i];
-            System.out.println("Actual Loss :"+actualLoss);
-            System.out.println("Calculated Loss :"+this.var[i]);
             if (this.var[i] > actualLoss) {
                 numberOfExceptions++;
             }
