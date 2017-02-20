4c5ce403694e0d3589e46b2a46ac22dd4c15d65d
dilini-mampitiya <dilu92arachchi@gmail.com>
2017-02-20 16:01:13 +0530

    modified historical simulation

--- src/main/java/org/wso2/siddhi/extension/var/models/util/asset/HistoricalAsset.java
+++ src/main/java/org/wso2/siddhi/extension/var/models/util/asset/HistoricalAsset.java
@@ -4,27 +4,26 @@ package org.wso2.siddhi.extension.var.models.util.asset;
  * Created by dilini92 on 1/9/17.
  */
 public class HistoricalAsset extends Asset {
-
-    private double previousLossReturn;
-    private double currentLossReturn;
+    private double[] previousSimulatedPriceList;
+    private double[] currentSimulatedPriceList;
 
     public HistoricalAsset(int windowSize) {
         super(windowSize);
     }
 
-    public double getPreviousReturnValue() {
-        return previousLossReturn;
+    public double[] getPreviousSimulatedPriceList() {
+        return previousSimulatedPriceList;
     }
 
-    public void setPreviousReturnValue(double previousLossReturn) {
-        this.previousLossReturn = previousLossReturn;
+    public double[] getCurrentSimulatedPriceList() {
+        return currentSimulatedPriceList;
     }
 
-    public double getCurrentReturnValue() {
-        return currentLossReturn;
+    public void setPreviousSimulatedPriceList(double[] previousSimulatedPriceList) {
+        this.previousSimulatedPriceList = previousSimulatedPriceList;
     }
 
-    public void setCurrentReturnValue(double currentLossReturn) {
-        this.currentLossReturn = currentLossReturn;
+    public void setCurrentSimulatedPriceList(double[] currentSimulatedPriceList) {
+        this.currentSimulatedPriceList = currentSimulatedPriceList;
     }
 }
